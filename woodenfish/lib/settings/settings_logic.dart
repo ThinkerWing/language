d30b1c9c4ee6963282fc84fc95360ca7b664c4b5
ThinkerWing <1099256274@qq.com>
2022-12-05 10:16:58 +0800

    feat: woodenfish

---
+++ woodenfish/lib/settings/settings_logic.dart
@@ -0,0 +1,43 @@
+import 'package:clipboard/clipboard.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:wooden_fish/settings/settings.dart';
+
+import 'settings_box.dart';
+
+class SettingsLogic extends GetxController {
+  Settings settings = SettingsBox.get();
+  TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void onInit() {
+    super.onInit();
+    textEditingController.text = settings.text;
+  }
+
+  save() {
+    settings.text = textEditingController.value.text;
+    settings.save();
+    Get.back(result: settings);
+  }
+
+  void updateInterval(double value) {
+    var valueInMills = value * 1000;
+    settings.interval = valueInMills ~/ 100 * 100;
+    update();
+  }
+
+  void updateAuto(bool value) {
+    settings.auto = value;
+    update();
+  }
+
+  void share(BuildContext context) {
+    FlutterClipboard.copy('https://github.com/ThinkerWing/language').then((value) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text("已复制下载链接，快去粘贴分享给好友吧！"),
+        duration: Duration(seconds: 5),
+      ));
+    });
+  }
+}
